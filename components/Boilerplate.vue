a3d63881ba48eaa0ad3c0b522b31600276460b97
Sergey <memonanofsa@gmail.com>
2018-08-14 16:40:36 +0500

    Добавил boilerplate

---
+++ components/Boilerplate.vue
@@ -0,0 +1,40 @@
+<template>
+  <div/>
+</template>
+
+<script>
+export default {
+  name: 'Boilerplate',
+  components: {},
+  mixins: [],
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  beforeCreate() {},
+  created() {},
+  fetch() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  activated() {},
+  deactivated() {},
+  beforeDestroy() {},
+  destroyed() {},
+  methods: {},
+  errorCaptured() {},
+};
+</script>
+
+<style lang="sass">
+  @import '@/assets/sass/common.sass'
+</style>
+
+<docs>
+```
+<Boilerplate/>
+```
+</docs>
