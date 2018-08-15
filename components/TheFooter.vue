f4d885d96b4782f58470790c09418d2e551e9b8c
Sergey <memonanofsa@gmail.com>
2018-08-15 17:40:50 +0500

    Создал компонент footer

---
+++ components/TheFooter.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="footer__container">
+    <div class="container-fluid d-flex justify-content-between align-items-center h-100">
+      <div class="footer__copy">2018 © X-WIN — будь первым!</div>
+      <div class="footer__links">
+        <a
+          href="/"
+          class="text--link text--accent"
+        >
+          Пользовательское соглашение
+        </a>
+        <a
+          href="/"
+          class="text--link text--accent"
+        >
+          Политика конфиденциальности
+        </a>
+      </div>
+      <div class="footer__btn--vk">
+        <IconBase
+          name="vk"
+          size="small"
+        />
+        <div class="text--link">Мы в Вконтакте</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import IconBase from '@/components/IconBase.vue';
+/**
+ * Компонент фотера
+ */
+export default {
+  name: 'TheFooter',
+  components: {
+    IconBase,
+  },
+  mixins: [],
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  beforeCreate() {},
+  created() {},
+  fetch() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  activated() {},
+  deactivated() {},
+  beforeDestroy() {},
+  destroyed() {},
+  methods: {},
+  errorCaptured() {},
+};
+</script>
+
+<style lang="sass">
+  @import '@/assets/sass/common.sass'
+  .footer__container
+    position: absolute
+    bottom: 0
+    left: 0
+    width: 100%
+    height: 69px
+    border-top: 1px solid rgba($primary-color, .15)
+  .footer__copy
+    font-size: 20px
+
+  .footer__links
+    display: flex
+    flex-flow: column nowrap
+
+  .footer__btn--vk
+    display: flex
+    align-items: center
+    height: 100%
+    padding: 0 25px
+    border-left: 1px solid rgba($primary-color, .15)
+    border-right: 1px solid rgba($primary-color, .15)
+    .text--link
+      margin-left: 5px
+</style>
+
+<docs>
+```
+<TheFooter/>
+```
+</docs>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,16 +3,19 @@
     <TheHeader/>
     <TheAside/>
     <nuxt/>
+    <TheFooter/>
   </div>
 </template>
 
 <script>
   import TheHeader from '@/components/TheHeader.vue';
   import TheAside from '@/components/TheAside.vue';
+  import TheFooter from '@/components/TheFooter.vue';
   export default {
     components: {
       TheHeader,
       TheAside,
+      TheFooter,
     },
   };
 </script>
@@ -20,5 +23,9 @@
 <style lang="sass">
   @import "normalize.css"
   @import "@/assets/sass/common.sass"
+  .container
+    position: relative
+    min-height: 100vh
+    padding-bottom: 69px
 </style>
 
