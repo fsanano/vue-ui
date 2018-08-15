632cef5cc21d9b0d8f5db4ef671f56ad0a7acf1f
Sergey <memonanofsa@gmail.com>
2018-08-15 14:32:03 +0500

    Добавил компонент регистрации и компонент акции

--- components/AsideSignup.vue
+++ components/AsideSignup.vue
@@ -4,12 +4,29 @@
     title="Регистарция"
     name="signup"
   >
-    signin
+    <form class="auth__container">
+      <div class="mb-15">
+        <InputText
+          v-model="email"
+          title="Электронная почта"
+        />
+      </div>
+      <ButtonPrimary>Регистрация</ButtonPrimary>
+    </form>
+
+    <AsideSocial/>
+    <AsideStock>
+      Зарегистрируйся и получи одну игру бесплатно!
+    </AsideStock>
   </AsideBase>
 </template>
 
 <script>
 import AsideBase from '@/components/AsideBase.vue';
+import InputText from '@/components/InputText.vue';
+import ButtonPrimary from '@/components/ButtonPrimary.vue';
+import AsideSocial from '@/components/AsideSocial.vue';
+import AsideStock from '@/components/AsideStock.vue';
 /**
  * Компонент сайдбара для регистрации
  */
@@ -17,6 +34,10 @@ export default {
   name: 'AsideSignup',
   components: {
     AsideBase,
+    InputText,
+    ButtonPrimary,
+    AsideSocial,
+    AsideStock,
   },
   mixins: [],
   props: {
---
+++ components/AsideStock.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="stock__container">
+    <div
+      class="stock__title"
+      v-html="title"
+    />
+    <div class="stock__content">
+      <!-- @slot Текст акции -->
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AsideStock',
+  props: {
+    title: {
+      type: String,
+      required: false,
+      default: 'Акция!',
+    },
+  },
+};
+</script>
+
+<style lang="sass">
+  @import '@/assets/sass/vars.sass'
+  .stock__container
+    margin: 0 30px
+    padding: 30px 25px
+    max-width: 328px
+    background: #1d1721
+
+  .stock__title
+    margin-bottom: 5px
+    font-family: 'Montserrat', sans-serif
+    font-size: 26px
+    font-weight: 700
+    color: $accent
+
+  .stock__content
+    font-size: 18px
+    line-height: 1.5
+    font-weight: 300
+
+</style>
+
+<docs>
+```
+<AsideStock>Текст акции</AsideStock>
+```
+</docs>
