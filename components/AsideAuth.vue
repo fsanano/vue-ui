a915a446ade616d593254d8ee31fd7bfe8ce6acf
Sergey <memonanofsa@gmail.com>
2018-08-16 10:53:28 +0500

    Создал общий компонент для входа и регистрации с подтверждением кода и отсчетом времени

---
+++ components/AsideAuth.vue
@@ -0,0 +1,144 @@
+<template>
+  <AsideBase
+    :name="name"
+    :title="title"
+    :status="status"
+  >
+    <form
+      v-if="isVisisbleForm"
+      class="auth__container"
+      @submit.prevent="confirmCode"
+    >
+      <div class="d-flex align-items-center mb-15">
+        <InputCode
+          v-model="code"
+          title="Код"
+          class="mr-10"
+        />
+        <ButtonLight
+          :is-disabled="countdown > 0"
+          typeof="code"
+          @click.prevent.native="startCountdown"
+        >
+          <span
+            v-if="countdown"
+            class="btn__countdown"
+            v-html="countdown"
+          />
+          Отправить еще раз
+        </ButtonLight>
+      </div>
+      <ButtonPrimary
+        class="mb-15"
+        typeof="full"
+      >
+        Продолжить
+      </ButtonPrimary>
+      <ButtonLight typeof="full">Ввести другой email</ButtonLight>
+
+    </form>
+    <template v-else>
+      <!-- @slot -->
+      <slot :signIn="signIn"/>
+    </template>
+  </AsideBase>
+</template>
+
+<script>
+import AsideBase from '@/components/AsideBase.vue';
+import InputCode from '@/components/InputCode.vue';
+import ButtonPrimary from '@/components/ButtonPrimary.vue';
+import ButtonLight from '@/components/ButtonLight.vue';
+/**
+ * Компонент обертка для комопнентов входа и регистрации
+ */
+export default {
+  name: 'AsideAuth',
+  components: {
+    AsideBase,
+    InputCode,
+    ButtonPrimary,
+    ButtonLight,
+  },
+  mixins: [],
+  props: {
+    /**
+     * Флаг видимости сайдбара
+     */
+    status: {
+      type: Boolean,
+      require: true,
+      default: false,
+    },
+    /**
+     * Заголовок сайдбара
+     */
+    title: {
+      type: String,
+      require: true,
+      default: '',
+    },
+    /**
+     * Название сайдбара
+     */
+    name: {
+      type: String,
+      require: true,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      /**
+       * Код подтверждения
+       * @type {String}
+       */
+      code: '',
+      /**
+       * Таймер отсчета
+       * @type {Number}
+       */
+      countdown: 60,
+      /**
+       * Флаг состояния формы
+       * @type {Boolean}
+       */
+      isVisisbleForm: false,
+    };
+  },
+  methods: {
+    signIn() {
+      this.isVisisbleForm = true;
+      this.startCountdown();
+    },
+    confirmCode() {
+      this.isVisisbleForm = false;
+    },
+    startCountdown() {
+      this.countdown = 60;
+      const interval = setInterval(() => {
+        if (this.countdown === 0) {
+          clearInterval(interval);
+        } else {
+          this.countdown = this.countdown - 1;
+        }
+      }, 1000);
+    },
+  },
+};
+</script>
+
+<style lang="sass">
+  @import '@/assets/sass/vars.sass'
+  .btn__countdown
+    width: 100%
+    margin-bottom: 5px
+    font-size: 16px
+    color: white
+</style>
+
+<docs>
+```
+<AsideAuth/>
+```
+</docs>
--- components/AsideSignin.vue
+++ components/AsideSignin.vue
@@ -1,67 +1,32 @@
 <template>
-  <AsideBase
+  <AsideAuth
     :status="status"
     title="Вход"
     name="signin"
   >
     <form
-      v-if="emailSended"
+      slot-scope="scope"
       class="auth__container"
-      @submit.prevent="confirmCode"
+      @submit.prevent="scope.signIn"
     >
-      <div class="d-flex align-items-center mb-15">
-        <InputCode
-          v-model="code"
-          title="Код"
-          class="mr-10"
+      <div class="mb-15">
+        <InputText
+          v-model="email"
+          title="Электронная почта"
         />
-        <ButtonLight
-          :is-disabled="countdown > 0"
-          typeof="code"
-          @click.prevent.native="startCountdown"
-        >
-          <span
-            v-if="countdown"
-            class="btn__countdown"
-            v-html="countdown"
-          />
-          Отправить еще раз
-        </ButtonLight>
       </div>
-      <ButtonPrimary
-        class="mb-15"
-        typeof="full"
-      >
-        Продолжить
-      </ButtonPrimary>
-      <ButtonLight typeof="full">Ввести другой email</ButtonLight>
-
+      <ButtonPrimary>Войти</ButtonPrimary>
     </form>
-    <template v-else>
-      <form
-        class="auth__container"
-        @submit.prevent="signIn"
-      >
-        <div class="mb-15">
-          <InputText
-            v-model="email"
-            title="Электронная почта"
-          />
-        </div>
-        <ButtonPrimary>Войти</ButtonPrimary>
-      </form>
 
-      <AsideSocial/>
-    </template>
-  </AsideBase>
+    <AsideSocial/>
+
+  </AsideAuth>
 </template>
 
 <script>
-import AsideBase from '@/components/AsideBase.vue';
+import AsideAuth from '@/components/AsideAuth.vue';
 import InputText from '@/components/InputText.vue';
-import InputCode from '@/components/InputCode.vue';
 import ButtonPrimary from '@/components/ButtonPrimary.vue';
-import ButtonLight from '@/components/ButtonLight.vue';
 import AsideSocial from '@/components/AsideSocial.vue';
 /**
  * Компонент сайдбара для входа
@@ -69,11 +34,9 @@ import AsideSocial from '@/components/AsideSocial.vue';
 export default {
   name: 'AsideSignin',
   components: {
-    AsideBase,
+    AsideAuth,
     InputText,
-    InputCode,
     ButtonPrimary,
-    ButtonLight,
     AsideSocial,
   },
   mixins: [],
@@ -91,49 +54,14 @@ export default {
        * @type {String}
        */
       email: '',
-      /**
-       * Код подтверждения
-       * @type {String}
-       */
-      code: '',
-
-      countdown: 60,
-      /**
-       * Флаг состояния формы
-       * @type {Boolean}
-       */
-      emailSended: false,
     };
   },
-  methods: {
-    signIn() {
-      this.emailSended = true;
-      this.startCountdown();
-    },
-    confirmCode() {
-      this.emailSended = false;
-    },
-    startCountdown() {
-      this.countdown = 60;
-      const interval = setInterval(() => {
-        if (this.countdown === 0) {
-          clearInterval(interval);
-        } else {
-          this.countdown = this.countdown - 1;
-        }
-      }, 1000);
-    },
-  },
+
 };
 </script>
 
 <style lang="sass">
   @import '@/assets/sass/vars.sass'
-  .btn__countdown
-    width: 100%
-    margin-bottom: 5px
-    font-size: 16px
-    color: white
 </style>
 
 <docs>
